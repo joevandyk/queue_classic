74656e1a312473237aaac6278bd615016c23fbfb
Joe Van Dyk <joe@fixieconsulting.com>
2013-03-11 13:17:19 -0700

    Use json type for the args column if json is available.

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,9 +1,22 @@
+-- If json type is available, use it for the args column.
+-- Otherwise, use text
+do $$
+declare
+  args_type text := 'text';
+begin
+  perform * from pg_type where typname = 'json';
+  if found then
+    args_type := 'json';
+  end if;
+
+  execute '
 CREATE TABLE queue_classic_jobs (
   id bigserial PRIMARY KEY,
-  q_name varchar(255),
-  method varchar(255),
-  args text,
+  q_name text not null,
+  method text not null,
+  args ' || args_type || ' not null,
   locked_at timestamptz
-);
+); ';
+end $$ language plpgsql;
 
 CREATE INDEX idx_qc_on_name_only_unlocked ON queue_classic_jobs (q_name, id) WHERE locked_at IS NULL;
